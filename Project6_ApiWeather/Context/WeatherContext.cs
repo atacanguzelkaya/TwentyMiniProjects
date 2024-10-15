60e6d145cd77de9ce8ef243975612044af812fcb
atacanguzelkaya <atacangzlky@gmail.com>
2024-10-15 17:09:47 +0300

    Added Project6_ApiWeather

---
+++ Project6_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+	public class WeatherContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Initial Catalog=Db6Project20; Integrated Security=true; TrustServerCertificate=True");
+		}
+		public DbSet<City> Cities { get; set; }
+	}
+}
