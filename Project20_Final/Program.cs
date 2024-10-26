48950ebd19b4e07e230f49f8ab06fd74b158bff3
atacanguzelkaya <atacangzlky@gmail.com>
2024-10-26 18:33:09 +0300

    Added Project20_Final

---
+++ Project20_Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project20_Final
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Training has ended.");
+        }
+	}
+}
